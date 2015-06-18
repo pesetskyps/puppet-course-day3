74fb2ee64e375a6bf6102b51bb33c26901e917a7
root <root@puppet.minsk.epam.com>
2015-06-18 09:24:31 -0400

    edit node to default 2

--- environments/production/manifests/site.pp
+++ environments/production/manifests/site.pp
@@ -1,6 +1,6 @@
 $powershell = 'C:\Windows\System32\WindowsPowerShell\v1.0\Powershell.exe -ExecutionPolicy RemoteSigned -noprofile -nologo -noninteractive -command'
 
-node 'dsccl1.hosting.ad.viacom.com' {
+node default {
   include common
   include ps_web::iis
   include ps_web::mysite
